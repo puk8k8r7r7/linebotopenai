6ee3384af6bddcd3939d3a20243155fba1c71c85
puk8k8r7r7 <77508542+puk8k8r7r7@users.noreply.github.com>
2023-11-30 04:53:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
 def get(city):
     token = 'CWA-4A8C2179-9849-40EB-947F-FD750B13862E'
-    url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={token}&format=JSON&locationName={str(city)}'
+    url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={token}&format=JSON&locationName={str(city)}'
     Data = requests.get(url)
     Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
     res = [[] , [] , []]
