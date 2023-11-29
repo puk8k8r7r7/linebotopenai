e8b033b33c3a0af77be62dcd28972c6d7fdd8e67
puk8k8r7r7 <77508542+puk8k8r7r7@users.noreply.github.com>
2023-11-30 06:56:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,99 @@
-from flask import Flask, request, abort, render_template
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import *
-import json
-import requests
-import openai
+import json,requests
+
+app = Flask(__name__)
+# LINE BOT info
+line_bot_api = LineBotApi('Channel Access Token')
+handler = WebhookHandler('Channel Secret')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    print(body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+
+def get(city):
+    token = 'Token'
+    url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
+    Data = requests.get(url)
+    Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
+    res = json.load(open('card.json','r',encoding='utf-8'))
+    for j in range(3):
+        bubble = json.load(open('bubble.json','r',encoding='utf-8'))
+        # title
+        bubble['body']['contents'][0]['text'] = city + '未來 36 小時天氣'
+        # time
+        bubble['body']['contents'][1]['contents'][0]['text'] = '{} ~ {}'.format(Data[0]['time'][j]['startTime'][5:-3],Data[0]['time'][j]['endTime'][5:-3])
+        # weather
+        bubble['body']['contents'][3]['contents'][1]['contents'][1]['text'] = Data[0]['time'][j]['parameter']['parameterName']
+        # temp
+        bubble['body']['contents'][3]['contents'][2]['contents'][1]['text'] = '{}°C ~ {}°C'.format(Data[2]['time'][j]['parameter']['parameterName'],Data[4]['time'][j]['parameter']['parameterName'])
+        # rain
+        bubble['body']['contents'][3]['contents'][3]['contents'][1]['text'] = Data[1]['time'][j]['parameter']['parameterName']
+        # comfort
+        bubble['body']['contents'][3]['contents'][4]['contents'][1]['text'] = Data[3]['time'][j]['parameter']['parameterName']
+        res['contents'].append(bubble)
+    return res
+
+# Message event
+@handler.add(MessageEvent)
+def handle_message(event):
+    message_type = event.message.type
+    user_id = event.source.user_id
+    reply_token = event.reply_token
+    if(message_type == 'text'):
+        message = event.message.text
+        if(message[:2] == '天氣'):
+            city = message[3:]
+            city = city.replace('台','臺')
+            if(not (city in cities)):
+                line_bot_api.reply_message(reply_token,TextSendMessage(text="查詢格式為: 天氣 縣市"))
+            else:
+                res = get(city)
+                print(res)
+                line_bot_api.reply_message(reply_token, FlexSendMessage(city + '未來 36 小時天氣預測',res))
+        else:
+            line_bot_api.reply_message(reply_token, TextSendMessage(text=message))
+    elif(message_type == 'location'):
+        city = event.message.address[5:8].replace('台','臺')
+        res = get(city)
+        line_bot_api.reply_message(reply_token, FlexSendMessage(city + '未來 36 小時天氣預測',res))
 import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import openai
+import time
+import traceback
+#======python的函數庫==========
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
@@ -15,20 +103,8 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
 openai.api_key = os.getenv('OPENAI_API_KEY')
-# 空氣汙染 API Key
-air_pollution_api_key = '1710a1b3-c964-41ad-a1e8-2d7705d5bc84'  # 您提供的金鑰
-# 支援的城市列表
-cities = ['基隆市', '嘉義市', '臺北市', '嘉義縣', '新北市', '臺南市', '桃園縣', '高雄市', '新竹市', '屏東縣', '新竹縣', '臺東縣', '苗栗縣', '花蓮縣', '臺中市']
 
-# 空氣品質查詢函數
-def get_air_quality(city):
-    url = f'https://api.openweathermap.org/data/2.5/air_pollution?q={city}&appid={air_pollution_api_key}'
-    response = requests.get(url)
-    data = response.json()
-    air_quality = data['list'][0]['main']['aqi']
-    return air_quality
 
-# OPENAI GPT 回應函數
 def GPT_response(text):
     # 接收回應
     response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
@@ -56,24 +132,34 @@ def callback():
         abort(400)
     return 'OK'
 
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    
-    if msg.startswith('天氣'):
-        city = msg[3:]
-        city = city.replace('台', '臺')
-        if city in cities:
-            air_quality = get_air_quality(city)
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{city}的空氣品質指數（AQI）為：{air_quality}"))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="查詢格式為: 天氣 縣市"))
-    else:
-        GPT_answer = GPT_response(msg)
-        print(GPT_answer)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    GPT_answer = GPT_response(msg)
+    print(GPT_answer)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
 
+        
+
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
+
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+        
+        
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+保留具有的功能 合併這個程式碼
