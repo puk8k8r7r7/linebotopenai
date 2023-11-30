183ae7a10207195a08abf61b05358f50f1ecfcbf
puk8k8r7r7 <77508542+puk8k8r7r7@users.noreply.github.com>
2023-11-30 10:41:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import requests
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,51 +7,67 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import os
-import openai
 
+import tempfile, os
+import datetime
+import openai
+import time
+import traceback
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# 故事 API 的 URL
-story_api_url = 'https://api.topthink.com/wiki/story'
 
-def get_random_story():
+def GPT_response(text):
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
+    
+    answer = response['choices'][0]['text'].replace('。', '')
+    
+    answer = answer.lstrip('?').lstrip()
+    
+    return answer
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
     try:
-        response = requests.get(story_api_url)
-        story_data = response.json()  # 假設 API 回傳的是 JSON 格式的故事列表
-        # 從回傳的故事中隨機選擇一個
-        story = random.choice(story_data['data'])
-        return story['title'] + '\n' + story['content']
-    except Exception as e:
-        print(f"取得故事時發生錯誤: {e}")
-        return None
-
-# ...
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if '健康小幫手' in msg:
-        health_info = health_assistant(msg)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(health_info))
-    elif '故事' in msg:
-        # 使用故事功能
-        story = get_random_story()
-        if story:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(story))
-        else:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage("無法取得故事，請稍後再試。"))
-    else:
-        GPT_answer = GPT_response(msg)
-        print(GPT_answer)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    GPT_answer = GPT_response(msg)
+    print(GPT_answer)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
 
-# ...
+        
 
+@handler.add(PostbackEvent)
+def handle_message(event):
+    print(event.postback.data)
+
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+        
+        
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
